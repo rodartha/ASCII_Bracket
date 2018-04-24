4d13a2bdb04c8064874bf3929a5d0c7b99bcb21b
Colin Page <cwpage@umich.edu>
2018-04-24 15:44:51 -0400

    Began to flesh out bracket class

--- Bracket/Bracket/Bracket.h
+++ Bracket/Bracket/Bracket.h
@@ -16,16 +16,51 @@
 #define BRACKET_H_
 
 #include<fstream>
+#include <vector>
 
 #include "Bye.h"
 #include "Basic_Text.h"
 
+using namespace std;
+
+// Keeps track of what type of bracket this is:
+enum class Type_of_Entry
+{
+    Basic_Text,
+    Song
+};
+
 class Bracket
 {
 public:
-    Bracket() {}
+    Bracket() : Bracket("ERROR", Type_of_Entry::Basic_Text) {}
 
-    ~Bracket() {}
+    Bracket(string f, Type_of_Entry e) : filename(f), entry_type(e) {}
+
+    // Main Function, handles all abilities of bracket class:
+    void run_bracket()
+    {
+        read_file();
+    }
+
+    ~Bracket() 
+    {
+        for (size_t i = 0; i < Entries.size(); ++i)
+        {
+            delete Entries[i];
+            Entries[i] = nullptr;
+        }
+    }
+private:
+    vector <Entry_Type*> Entries;
+    string filename;
+    Type_of_Entry entry_type;
+
+    // Reads the file into Entries:
+    void read_file()
+    {
+
+    }
 };
 
 #endif
