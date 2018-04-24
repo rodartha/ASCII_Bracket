15854c741ba7791f9c97303d0037f393f7934909
Colin Page <cwpage@umich.edu>
2018-04-24 15:27:32 -0400

    Created Bracket Class

---
+++ Bracket/Bracket/Bracket.h
@@ -0,0 +1,31 @@
+/**
+* Bracket.h
+*
+* ASCII Bracket Program
+* Colin Page
+* cwpage@umich.edu
+*
+* Version: 0.0
+* Last Edited: 4/24/18
+*
+* This file holds the Bracket class which contains the bracket itself and
+* handles the main processes of the program.
+*/
+
+#ifndef BRACKET_H_
+#define BRACKET_H_
+
+#include<fstream>
+
+#include "Bye.h"
+#include "Basic_Text.h"
+
+class Bracket
+{
+public:
+    Bracket() {}
+
+    ~Bracket() {}
+};
+
+#endif
